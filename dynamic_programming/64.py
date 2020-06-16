41e7f6e96e23b9384f5c33c9b7516252a88aae5a
zhangyan <18758266469@163.com>
2020-06-16 09:51:25 +0800

    add 64 714

---
+++ dynamic_programming/64.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# !@time: 2020-06-16 08:31
+# !@author: superMC @email: 18758266469@163.com
+# !@fileName: 64.py
+'''给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+
+说明：每次只能向下或者向右移动一步。'''
+from typing import List
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                    continue
+                elif i == 0:
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i][j - 1] + grid[i][j], dp[i - 1][j] + grid[i][j])
+
+        return dp[m - 1][n - 1]
+
+
+if __name__ == '__main__':
+    grid = [
+        [1, 3, 1],
+        [1, 5, 1],
+        [4, 2, 1]
+    ]
+    print(Solution.minPathSum(super, grid))
---
+++ dynamic_programming/714.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# !@time: 2020-06-16 08:56
+# !@author: superMC @email: 18758266469@163.com
+# !@fileName: 714.py
+
+'''给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；非负整数 fee 代表了交易股票的手续费用。
+
+你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+
+返回获得利润的最大值。
+
+注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+'''
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        days = len(prices)
+        # 构造状态转移方程
+        states = 2
+        dp = [[0] * states for _ in range(days)]
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        for i in range(1, days):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
+            dp[i][1] = max(dp[i - 1][0] - prices[i], dp[i - 1][1])
+        return dp[days - 1][0]
+
+
+if __name__ == '__main__':
+    prices = [1, 3, 2, 8, 4, 9]
+    fee = 2
+    print(Solution.maxProfit(super, prices, fee))
