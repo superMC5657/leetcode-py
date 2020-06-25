b71f86bafec080299ddde1cd03948a450824d5bd
zhangyan <18758266469@163.com>
2020-06-25 12:51:34 +0800

    uncompleted 0811 1477

--- dynamic_programming/0811.py
+++ dynamic_programming/0811.py
@@ -2,6 +2,9 @@
 # !@time: 2020/6/17 14 01
 # !@author: superMC @email: 18758266469@163.com
 # !@fileName: 0811.py
+"""硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)"""
+
+
 class Solution:
     def waysToChange(self, n: int) -> int:
         coins = [25, 10, 5, 1]
@@ -21,11 +24,19 @@ class Solution:
             dp[i][0] = 1
         for j in range(n + 1):
             dp[0][j] = 0
-        for i in range(len(coins)+1):
+        for i in range(len(coins) + 1):
             for j in range(coins[i], n + 1):
                 dp[j] += dp[j - coins[i]]
         return dp[-1] % 1000000007
 
+    def waysToChange_math(self, n: int) -> int:
+        n = n // 5
+        ans = 0
+        while n >= 0:
+            ans = (ans + (n // 2 + 1) * ((n + 1) // 2 + 1))
+            n -= 5
+        return ans % 1000000007
+
 
 if __name__ == '__main__':
-    print(Solution().waysToChange_dp(5))
+    print(Solution().waysToChange_math(10000))
---
+++ dynamic_programming/1477.py
@@ -0,0 +1,18 @@
+        # -*- coding: utf-8 -*-
+        # !@time: 2020-06-20 20:47
+        # !@author: superMC @email: 18758266469@163.com
+        # !@fileName: 1477.py
+        from typing import List
+
+        """
+        给你一个整数数组 arr 和一个整数值 target 。
+        请你在 arr 中找 两个互不重叠的子数组 且它们的和都等于 target 。
+        可能会有多种方案，请你返回满足要求的两个子数组长度和的 最小值 。
+        请返回满足要求的最小长度和，如果无法找到这样的两个子数组，请返回 -1 。
+        """
+
+        class Solution:
+            def minSumOfLengths(self, arr: List[int], target: int) -> int:
+
+
+
